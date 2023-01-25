f41b2a60aae20f6ff9d661759fbe665e87513645
Tutul Kabir <tutulkabir@gmail.com>
2023-01-25 20:44:08 +0530

    adding style

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import style from "./countries.module.css";
 import { v4 as uuidv4 } from "uuid";
 import Country from "./Country";
 const Countries = (props) => {
   return (
-    <section>
+    <section className={style.countries}>
       {props.countries.map((country) => {
         const countryNew = { country, id: uuidv4() };
         return <Country {...countryNew} key={countryNew.id} />;
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+import style from "./country.module.css";
 const Country = (props) => {
   const { name, flags, capital, area, population } = props.country;
   return (
-    <article>
+    <article className={style.country}>
       <div>
-        <img src={flags.png} alt={name.common} />
+        <img className={style.flag} src={flags.png} alt={name.common} />
         <h3>Name:{name.common}</h3>
         <h3>Capital: {capital}</h3>
         <h3>Population: {population}</h3>
